c0909e0bd1257246211ca8b1c8515868c3ea2eee
Vaibhav <vaibhavranjan420@gmail.com>
2025-06-24 18:14:10 +0530

    aise hi

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+import { Children } from "react";
+import Navbar from "../components/Navbar";
+
+export default function Layout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <>
+      <main className="font-work-sans">
+        <Navbar />
+        {children}
+      </main>
+    </>
+  );
+}
---
+++ app/components/ClientNav.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Link from "next/link";
+import { signIn, signOut } from "next-auth/react";
+
+const ClientNav = ({ session }: { session: any }) => {
+  return (
+    <div className="flex items-center gap-5 text-black">
+      {session?.user ? (
+        <>
+          <Link href="/startup/create">
+            <span>Create</span>
+          </Link>
+          <button onClick={() => signOut({ callbackUrl: "/" })}>
+            <span>Logout</span>
+          </button>
+          <Link href={`/user/${session?.id}`}>
+            <span>{session?.user.name}</span>
+          </Link>
+        </>
+      ) : (
+        <button onClick={() => signIn("github")}>
+          <span>Login</span>
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default ClientNav;
